68bd1c0816ce85b98c04efc784dd56e50d052c20
joq62 <joakim.leche@gmail.com>
2022-01-16 12:04:07 +0100

    lib_bully ud+++

--- external_src/lib_bully.erl
+++ external_src/lib_bully.erl
@@ -33,15 +33,19 @@
 %% ====================================================================
 get_nodes()->
     {ok,KubeletNodes}=application:get_env(kubelet_nodes),
+    lists:delete(node(),KubeletNodes).
+
+%get_nodes()->
+ %   {ok,KubeletNodes}=application:get_env(kubelet_nodes),
   %  case  [Node||Node<-KubeletNodes,
 %		 pong=:=net_adm:ping(Node)] of
-    case sd:get(glurk) of
-	[]->
-	    rpc:cast(node(),log,log,[?Log_info("no kubelet nodes ",[])]),
-	    [];
-	Nodes ->
-	    rpc:cast(node(),log,log,[?Log_info("Nodes ",[Nodes])]),
-	    Nodes
-    end.
+ %   case sd:get(glurk) of
+%	[]->
+%	    rpc:cast(node(),log,log,[?Log_info("no kubelet nodes ",[])]),
+%	    [];
+%	Nodes ->
+%	    rpc:cast(node(),log,log,[?Log_info("Nodes ",[Nodes])]),
+%	    Nodes
+ %   end.
   % lists:delete(node(),sd:get(BullyApp)).
   %  lists:delete(node(),sd:get(bully_test)).
